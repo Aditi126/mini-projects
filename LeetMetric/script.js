b0fe40047c4a8c51a1a6e3f21274d937a932f40d
Aditi126 <adiime126@gmail.com>
2025-05-26 22:34:29 +0530

    Started adding js

---
+++ LeetMetric/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const searchButton = document.querySelector("#search-button");
+    const usernameInput = document.querySelector(".inp");
+    const easy = document.querySelector(".easy");
+    const medium = document.querySelector(".medium");
+    const hard = document.querySelector(".hard");
+    const easyLabel = document.querySelector(".easy p");
+    const mediumLabel = document.querySelector(".medium p");
+    const hardLabel = document.querySelector(".hard p");
+    const rank = document.querySelector("#rank");
+    const problems = document.querySelector("#problem-count");
+    const contests = document.querySelector("#contest-attended");
+    const rating = document.querySelector("#contest-rating");
+
+    
+})
